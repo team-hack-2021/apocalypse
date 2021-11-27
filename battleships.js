dffc48ee0e331b2a0fd24413804d73eb2f9972fb
Bradley Sansom <19352051+bradleysansom@users.noreply.github.com>
2021-11-27 20:22:16 +0000

    Update battleships.js

--- battleships.js
+++ battleships.js
@@ -20,3 +20,18 @@ const bounding_box_for_query = bounds_south + "%2C" + bounds_west + "%2c" + boun
 const overpass_query = "https://overpass-api.de/api/interpreter?data=%2F*%0AThis%20has%20been%20generated%20by%20the%20overpass-turbo%20wizard.%0AThe%20original%20search%20was%3A%0A%E2%80%9Cbuilding%20in%20bbox%E2%80%9D%0A*%2F%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%2F%2F%20gather%20results%0A%28%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9Cbuilding%E2%80%9D%0A%20%20way%5B%22building%22%5D%28" + bounding_box_for_query + "%29%3B%0A%20%20relation%5B%22building%22%5D%28" + bounding_box_for_query + "%29%3B%0A%29%3B%0A%2F%2F%20print%20results%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B";
 console.log(overpass_query);                                                                                            // save to console for debugging        
 document.getElementById('debug2').innerHTML = "<a href='" + overpass_query + "'>" + overpass_query + "</a>";            // display on page for debugging
+
+// fetch all buildings on map
+fetch(overpass_query)
+    .then(response => response.json())
+    .then(data => {
+        const all_buildings = data.elements;
+        console.log(all_buildings);
+
+
+        function random_building() {
+
+        }
+
+    });
+
